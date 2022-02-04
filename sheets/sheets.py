d5ebbe9e01bfe1168f18de64b3474a4aa9671337
Tim <herew26@gmail.com>
2022-02-04 23:53:17 +0300

    sheets updated

--- sheets/sheets.py
+++ sheets/sheets.py
@@ -2,13 +2,15 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 
-scope = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/sprea...","https://www.googleapis.com/auth/drive...","https://www.googleapis.com/auth/drive"]
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 
 creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
 
 client = gspread.authorize(creds)
 
-sheet = client.open("SewingTracker")
+sheet = client.open("SewingTracker").worksheets()[0]
+
+print(sheet.update_cell(2, 2, "TEST"))
 
 if __name__ == "__main__":
     pass
