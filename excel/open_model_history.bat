ce7ebb58a5528fbcfe0b19df3a26cbac60cb2461
Tim <herew26@gmail.com>
2022-02-08 12:50:18 +0300

    excel deleted

--- excel/open_model_history.bat
+++
@@ -1,3 +0,0 @@
-call cd %0\..\..
-call "venv\Scripts\activate.bat"
-call python app.py mode=model model=%1 market=%2
--- excel/update_excel.bat
+++
@@ -1,4 +0,0 @@
-call cd %0\..\..
-call "venv\Scripts\activate.bat"
-call python app.py mode=update_model market=%1
-call "excel\input.xlsm"
