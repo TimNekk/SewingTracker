89d34c54571b3faa525654b59f1808d5492aa96f
Tim <herew26@gmail.com>
2022-02-16 21:36:15 +0300

    ozon fix

--- parsing/websites/ozon/ozon.py
+++ parsing/websites/ozon/ozon.py
@@ -1,12 +1,14 @@
 import re
 from typing import Optional
 
+from fuzzywuzzy import fuzz
+
 from parsing.websites import Parser
 
 
 class OzonParser(Parser):
     def __init__(self):
-        self._price_selector = ".jm9 .m9j"
+        self._price_selector = "#layoutPage > div.f5z > div.container.f6z > div.jl.lj4 > div.jl.lj5.lj2.l2j > div.jl.lj5.lj2.jl3 > div > div > div > div:nth-child(1) > div > div > div.jq9.rj0.r1j > div > span.q9j.jr > span"
         self._base_url = "https://ozon.com"
         self._search_url = self._base_url + "/search/?from_global=true&text="
 
@@ -21,18 +23,18 @@ class OzonParser(Parser):
 
         soup = self._get_soup(url)
 
-        models_grid = soup.select(".gz7")
+        models_grid = soup.select("#layoutPage > div.f5z > div:nth-child(8) > div > div > div:nth-child(2) > div.ho9 > div > div > div > div:nth-child(1) > div.m8h > a")
 
         if not models_grid:
             return
 
         for model in models_grid:
             try:
-                model_name = model.text
-                if model_name.lower() not in search.lower():
-                    continue
-                model_url = self._base_url + model['href']
-                models[model_name] = model_url
+                model_name = re.sub(r"[а-яА-Я]+", "", model.text.split("[")[0]).strip()
+
+                if fuzz.ratio(model_name, search) >= 96:
+                    model_url = self._base_url + model['href']
+                    models[model_name] = model_url
             except Exception:
                 pass
 
