6df26a3a24758ac745986912f3e8d08f3bca3cad
TimNekk <herew26@gmail.com>
2022-02-16 12:16:48 +0300

    techport parsing bug fixed

--- parsing/websites/techport.py
+++ parsing/websites/techport.py
@@ -1,6 +1,6 @@
 import logging
 import re
-from typing import List
+from typing import List, Optional
 
 from fuzzywuzzy import fuzz
 
@@ -13,8 +13,9 @@ class TechPortParser(Parser):
         self._base_url = "https://www.techport.ru"
         self._search_url = self._base_url + "/q/?catid=20000&t="
 
-    def parse_model(self, url: str) -> int:
+    def parse_model(self, url: str) -> Optional[int]:
         soup = self._get_soup(url)
+        if soup.select_one(".tcp-list-group__link_dot").text == "Нет в наличии": return
         price = int("".join(re.findall(r"\d+", soup.select_one(self._price_selector).text)))
         return price
 
