2d9aa4b5e1fa87d43364dff5bcd996573795c714
TimNekk <herew26@gmail.com>
2022-02-26 14:50:32 +0300

    xpath added to ozon market sewing kingdom

--- parsing/websites/ozon/sewing_kingdom.py
+++ parsing/websites/ozon/sewing_kingdom.py
@@ -5,3 +5,4 @@ class SewingKingdomOzonParser(OzonParser):
     def __init__(self):
         super().__init__()
         self._search_url = self._base_url + "/seller/ooo-shveynoe-korolevstvo-147310/products/?from_global=true&text="
+        self._name_xpath = '//*[@id="layoutPage"]/div[1]/div[5]/div/div[2]/div[2]/div/div/div/div[1]/div[1]/a'
