fb1edf83c1bb52e0e61f8ba0f2bb68c44d04a041
TimNekk <herew26@gmail.com>
2022-02-27 16:57:52 +0300

    status enum added

---
+++ sheets/status.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class Status(Enum):
+    exporting_to_db = "Выгрузка моделей из таблиц"
+    parsing_market = "Поиск моделей в магазине"
+    parsing_model = "Парсинг модели"
+    exporting_to_sheets = "Загрузка цен в таблицы"
+    done = "Загрузка завершена"
